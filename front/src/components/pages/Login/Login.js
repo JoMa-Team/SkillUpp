f346a7134c9c8640b3e0e6b6a53deaaf49aa2c39
joannaberlin <joanna.kot.ok@gmail.com>
2023-03-07 10:02:46 +0100

    Add info to step-by-step

--- front/src/components/pages/Login/Login.js
+++ front/src/components/pages/Login/Login.js
@@ -1,5 +1,26 @@
+import Button from '../../common/Button/Button';
+import styles from './Login.module.scss';
+
 const Login = () => {
-	return <div>Here will be a form soon</div>;
+	return (
+		<div className={styles.form_wrapper}>
+			<form className={styles.form}>
+				<input
+					className={styles.loginInput}
+					type='email'
+					placeholder='email@email.com'
+				></input>
+				<input
+					className={styles.loginInput}
+					type='password'
+					placeholder='password'
+				></input>
+				<div className={styles.loginButton_wrapper}>
+					<Button type='submit'>Log in</Button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
--- front/src/components/pages/Signup/Signup.js
+++ front/src/components/pages/Signup/Signup.js
@@ -1,5 +1,36 @@
+import Button from '../../common/Button/Button';
+import styles from './Signup.module.scss';
+
 const Signup = () => {
-	return <div>Here will be a form soon</div>;
+	return (
+		<div className={styles.form_wrapper}>
+			<form className={styles.form}>
+				<input
+					className={styles.signupInput}
+					type='text'
+					placeholder='username'
+				></input>
+				<input
+					className={styles.signupInput}
+					type='email'
+					placeholder='email@email.com'
+				></input>
+				<input
+					className={styles.signupInput}
+					type='password'
+					placeholder='password'
+				></input>
+				<input
+					className={styles.signupInput}
+					type='password'
+					placeholder='repeat password'
+				></input>
+				<div className={styles.signupButton_wrapper}>
+					<Button type='submit'>Sign up</Button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Signup;
