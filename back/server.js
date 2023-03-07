96d7786cdac8527ea0cd89e5f48fe009f88afa92
Marina Tacheva <cookiesntech@gmail.com>
2023-03-07 10:45:20 +0100

    Set up port and database

--- back/server.js
+++ back/server.js
@@ -1,1 +1,13 @@
-import express from "express";
+const express = require("express");
+const app = express()
+//import mongoose from "mongoose";
+//import MongoStore from "connect-mongo";
+const connectDB = require("./config/database");
+
+require('dotenv').config({path: "./config/.env"});
+const PORT = process.env.PORT;
+connectDB()
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
